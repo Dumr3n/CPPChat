08d413328f51c53e4fa29866e37cc11ed68f2818
Dumren <k3nteck@gmail.com>
2026-06-08 12:04:42 +0300

    Server and client, basic structure

---
+++ client/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Client program" << std::endl;
+    return 0;
+}
--- server/main.cpp
+++ server/main.cpp
@@ -1,3 +1,6 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
 #include <iostream>
 #include <vector>
 #include <thread>
@@ -6,5 +9,28 @@
 
 int main()
 {
+    WSADATA wsaData{};
+
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    if (result != 0) {
+        std::cerr << "WSAStartup failed" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to create server socket" << std::endl;
+        WSACleanup();
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Server socket created successfully" << std::endl;
+
+    closesocket(serverSocket);
+    WSACleanup();
+    
+
     return 0;
 }
